32c058ba505fd2f98f8c33416450de2b0223f1ad
EOG <chocatus411@gmail.com>
2026-08-02 22:16:41 +0100

    contenu du frontend 1

---
+++ frontend/src/Components/UI/Button/button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./button.css"
+
+const button = () => {
+  return (
+    <div>button</div>
+  )
+}
+
+export default button
---
+++ frontend/src/Components/forms/LoginForm/loginform.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./loginform.css"
+
+const loginform = () => {
+  return (
+    <div>loginform</div>
+  )
+}
+
+export default loginform
